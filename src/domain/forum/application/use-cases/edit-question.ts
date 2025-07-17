ab3a0dcc610bd9eae936f120e1767d8c4e98c2d9
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-07-17 09:17:18 -0300

    feat: :sparkles: use case: edit answer

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,3 +1,4 @@
+import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 
 interface EditQuestionUseCaseRequest {
@@ -7,8 +8,9 @@ interface EditQuestionUseCaseRequest {
   content: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface EditQuestionUseCaseResponse {}
+interface EditQuestionUseCaseResponse {
+  question: Question;
+}
 
 export class EditQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
@@ -34,6 +36,8 @@ export class EditQuestionUseCase {
 
     await this.questionsRepository.save(question);
 
-    return {};
+    return {
+      question,
+    };
   }
 }
