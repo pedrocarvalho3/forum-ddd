eee71e86c6a9b4274e281986c2851f07d3638b5c
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-07-29 09:56:22 -0300

    feat: :sparkles: attachment entities

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,4 +1,4 @@
-import { AggregateRoot } from "@/core/entities/agregate-root";
+import { AggregateRoot } from "@/core/entities/aggregate-root";
 import { Slug } from "./value-objects/slug";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
