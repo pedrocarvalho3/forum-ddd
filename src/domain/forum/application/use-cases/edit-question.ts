f01096c884b8b6f0222180f33b3c1fbda189d3de
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-07-31 10:38:05 -0300

    feat: :sparkles: use-case: send notification

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,7 +1,7 @@
 import { Either, left, right } from "@/core/either";
 import { QuestionsRepository } from "../repositories/questions-repository";
-import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-import { NotAllowedError } from "./errors/not-allowed-error";
+import { ResourceNotFoundError } from "../../../../core/errors/errors/resource-not-found-error";
+import { NotAllowedError } from "../../../../core/errors/errors/not-allowed-error";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionAttachmentsRepository } from "../repositories/question-attachments-repository";
 import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
